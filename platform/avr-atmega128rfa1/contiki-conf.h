e64259503ee902c443aa895d20f7be96f3f2409b
Thomas Kühnel <kuehnelth@googlemail.com>
2013-06-11 14:23:19 +0200

    avr-atmega128rfa1: don't redefine UIP_CONF_TCP

--- platform/avr-atmega128rfa1/contiki-conf.h
+++ platform/avr-atmega128rfa1/contiki-conf.h
@@ -154,7 +154,9 @@ typedef unsigned short uip_stats_t;
 #define RIMEADDR_CONF_SIZE        8
 #define UIP_CONF_ICMP6            1
 #define UIP_CONF_UDP              1
+#ifndef UIP_CONF_TCP
 #define UIP_CONF_TCP              1
+#endif
 #define NETSTACK_CONF_NETWORK     sicslowpan_driver
 #define SICSLOWPAN_CONF_COMPRESSION SICSLOWPAN_COMPRESSION_HC06
 #else
