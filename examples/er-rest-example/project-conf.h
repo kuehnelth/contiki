3c6317be08ddf86ba2d4e85f52353e9952dedc79
Thomas Kühnel <kuehnelth@googlemail.com>
2014-02-02 14:39:21 +0100

    er-example-server: lower send/receive power for testing in project-conf.h

--- examples/er-rest-example/project-conf.h
+++ examples/er-rest-example/project-conf.h
@@ -91,4 +91,10 @@
 #undef UIP_CONF_ROUTER
 #define UIP_CONF_ROUTER 0
 
+//lower send/receive power for testing 
+/*
+#define RF230_MAX_TX_POWER 15
+#define RF230_MIN_RX_POWER 30
+*/
+
 #endif /* __PROJECT_ERBIUM_CONF_H__ */
